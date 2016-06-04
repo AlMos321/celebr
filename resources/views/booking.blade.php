953e50dcd99033a8f57cc2025b34726ead2a9a55
Alexander_M <alexmoss09@gmail.com>
2016-06-04 22:22:17 +0300

    sdfsdf

--- resources/views/booking.blade.php
+++ resources/views/booking.blade.php
@@ -528,7 +528,7 @@
                     </section>
                 </fieldset>
                 @else
-                    <label>Вибаріть приблизний час</label>
+                    <label style="text-align: center;">Вибаріть приблизний час</label>
                     <select class="form-control" id="hol_type" name="time_user" style="width: 25%; margin: 0 auto;">
                             <option id="" value="00">00</option>
                             <option id="" value="01">01</option>
